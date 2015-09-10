08634230cb73aaf57ffed6ca3a519a0f24fcc477
Erin <edooley7@gmail.com>
2015-09-10 13:43:18 -0400

    final model

--- run_model.py
+++ run_model.py
@@ -2,6 +2,7 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 from patsy import dmatrices
+
 pd.options.mode.chained_assignment = None
 from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
@@ -11,23 +12,77 @@ from sklearn.metrics import roc_curve, auc
 
 from sklearn.cross_validation import cross_val_score, train_test_split
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, RandomizedLogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-df = pd.DataFrame(pickle.load(open('clean_recent_data.pkl', 'rb')))
+df = pd.DataFrame(pickle.load(open('data_files/dummied_recent_data.pkl', 'rb')))
 print "Done reading dataframe"
 
+X = df[[
 
-y, X = dmatrices(
-    'RESP ~ teacher_previous_projects + school_previous_projects + primary_focus_area + primary_focus_subject + np.log(total_price_including_optional_support + np.sqrt(students_reached))',
-    data=df, return_type='dataframe')
+ 'log_price_including',
+ 'sqrt_students_reached',
+ 'price_per_student',
+ 'total_state_donors',
+ 'total_state_projects',
+ 'state_avg_donors',
+ 'primary_focus_subject_Applied Sciences',
+ 'primary_focus_subject_Character Education',
+ 'primary_focus_subject_Civics & Government',
+ 'primary_focus_subject_College & Career Prep',
+ 'primary_focus_subject_Community Service',
+ 'primary_focus_subject_ESL',
+ 'primary_focus_subject_Early Development',
+ 'primary_focus_subject_Economics',
+ 'primary_focus_subject_Environmental Science',
+ 'primary_focus_subject_Extracurricular',
+ 'primary_focus_subject_Financial Literacy',
+ 'primary_focus_subject_Foreign Languages',
+ 'primary_focus_subject_Gym & Fitness',
+ 'primary_focus_subject_Health & Life Science',
+ 'primary_focus_subject_Health & Wellness',
+ 'primary_focus_subject_History & Geography',
+ 'primary_focus_subject_Literacy',
+ 'primary_focus_subject_Literature & Writing',
+ 'primary_focus_subject_Mathematics',
+ 'primary_focus_subject_Music',
+ 'primary_focus_subject_Nutrition',
+ 'primary_focus_subject_Other',
+ 'primary_focus_subject_Parent Involvement',
+ 'primary_focus_subject_Performing Arts',
+ 'primary_focus_subject_Social Sciences',
+ 'primary_focus_subject_Special Needs',
+ 'primary_focus_subject_Team Sports',
+ 'primary_focus_subject_Visual Arts',
+ 'poverty_level_high poverty',
+ 'poverty_level_highest poverty',
+ 'poverty_level_low poverty',
+ 'poverty_level_moderate poverty',
+ 'grade_level_Grades 3-5',
+ 'grade_level_Grades 6-8',
+ 'grade_level_Grades 9-12',
+ 'grade_level_Grades PreK-2',
+ 'school_metro_rural',
+ 'school_metro_suburban',
+ 'school_metro_urban',
+ 'school_previous_projects',
+ 'teacher_previous_projects',
+ #'teacher_gender_Female',
+ #'teacher_gender_Male',
+ 'resource_type_Books',
+ 'resource_type_Other',
+ 'resource_type_Supplies',
+ 'resource_type_Technology',
+ 'resource_type_Trips',
+ 'resource_type_Visitors']]
+y = df[['RESP']]
 
 y = np.ravel(y)
 
@@ -39,25 +94,28 @@ x_train, x_test, y_train, y_test = train_test_split(
     X, y, test_size=0.25, random_state=7)
 print "Done splitting"
 
-def always_complete(x):
-    return [1] * len(x)
-y_pred = always_complete(X)
-
-print "Baseline = %0.2f" % accuracy_score(y, y_pred)
 
 def test_model(name, main_model):
     model = main_model
     model.fit(x_train, y_train)
-    precision = np.mean(cross_val_score(main_model, X, y.ravel(), scoring = 'precision'))
-    print name, "precision", precision
-    print name, "confusion matrix", confusion_matrix(y_test, model.predict(x_test))
+    precision = np.mean(cross_val_score(main_model, X, y.ravel(), scoring='precision'))
+    cm = confusion_matrix(y_test, model.predict(x_test))
+    print name, "Precision", precision
+    print name, "Percentage off", cm[0][1]/(cm[0][0]+cm[0][1])
 
-test_model("Logistic Regression", LogisticRegression())
-test_model("GaussianNB", GaussianNB())
-#test_model("MultinomialNB", MultinomialNB())
-test_model("DecisionTreeClassifier", DecisionTreeClassifier())
-test_model("RandomForestClassifier", RandomForestClassifier())
-test_model("GradientBoostingClassifier", GradientBoostingClassifier())
-test_model("KNeighborsClassifier", KNeighborsClassifier())
-test_model("SVC", SVC())
+test_model("RF auto weights", RandomForestClassifier(max_depth = 8, class_weight = "auto" ))
+test_model("DT", DecisionTreeClassifier(max_depth = 8, class_weight = "auto" ))
+test_model("DT", DecisionTreeClassifier(max_depth = 4, class_weight = "auto" ))
+
+
+#test_model("RLR", RandomizedLogisticRegression())
+# test_model("Bernoulli", BernoulliNB())
+# test_model("GradientBoostingClassifier", GradientBoostingClassifier(n_estimators=500))
+# test_model("ADA", AdaBoostClassifier())
+# test_model("Logistic Regression", LogisticRegression())
+# test_model("GaussianNB", GaussianNB())
+# test_model("MultinomialNB", MultinomialNB())
+# test_model("DecisionTreeClassifier", DecisionTreeClassifier())
+# test_model("KNeighborsClassifier", KNeighborsClassifier())
+# test_model("SVC", SVC())
 print "DONE"
